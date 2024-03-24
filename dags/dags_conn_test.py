c26601ef0f4695813f80d41a4c46eccc6f7f2436
nsyunogong99 <nsyunogong99@gmail.com>
2024-03-24 22:03:51 +0900

    feat: Add dags_conn_test dag

---
+++ dags/dags_conn_test.py
@@ -0,0 +1,33 @@
+import datetime
+import pendulum
+from airflow.operators.empty import EmptyOperator
+from airflow.models.dag import DAG
+
+with DAG(
+    dag_id="dags_conn_test",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    
+    t1 = EmptyOperator(
+        task_id="t1"
+    )
+
+    t2 = EmptyOperator(
+        task_id="t2"
+    )
+
+    t3 = EmptyOperator(
+        task_id="t3"
+    )
+
+    t4 = EmptyOperator(
+        task_id="t4"
+    )
+
+    t5 = EmptyOperator(
+        task_id="t5"
+    )
+
+    t1 >> [t2, t3] >> t4 << t5
