a28235064d1236915145e396eafebd7c26dad579
nsyunogong99 <nsyunogong99@gmail.com>
2024-03-24 22:59:24 +0900

    fix: Fix dags_bash_select_fruit dag invalid syntax

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -5,18 +5,18 @@ from airflow.models.dag import DAG
 
 with DAG(
     dag_id="dags_bash_operator",
-    schedule="0 0 * * *",
+    schedule="10 0 * * 6#1",
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
 ) as dag:
     
     t1_orange = BashOperator(
-        task_id="t1_orange"
+        task_id="t1_orange",
         bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE"
     )
 
     t2_avocado = BashOperator(
-        task_id="t2_avocado"
+        task_id="t2_avocado",
         bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO"
     )
 
